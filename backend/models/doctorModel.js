d01f079149c9933129919bd8c2bef95fe9598b84
Ismaeil Saberi <ismaeilsaberi@gmail.com>
2024-09-30 22:11:37 -0700

    adding cloudinary config for storing images and creating doctorModel on backend

---
+++ backend/models/doctorModel.js
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+
+const doctorSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    speciality: {
+      type: String,
+      required: true,
+    },
+    degree: {
+      type: String,
+      required: true,
+    },
+    experience: {
+      type: String,
+      required: true,
+    },
+    about: {
+      type: String,
+      required: true,
+    },
+    available: {
+      type: Boolean,
+      required: true,
+    },
+    fees: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Number,
+      required: true,
+    },
+    slots_booked: { type: Object, default: {} },
+  },
+  //// the following code minimize is for adding default object value for the few properties of model
+  { minimize: false }
+);
+
+const doctorModel =
+  mongoose.models.doctor || mongoose.model("doctor", doctorSchema);
+
+export default doctorModel;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./config/mongodb.js ";
+import connectCloudinary from "./config/cloudinary.js";
 
 // app config
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
 
 // middlewares
 app.use(express.json());
