fef07b11d02e810df915564f19b3f2c3aea4c73f
Ismaeil Saberi <ismaeilsaberi@gmail.com>
2024-10-07 11:18:37 -0700

    adminLogin controller and adminAuth middleware added to the project successfully

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,6 +2,7 @@ import validator from "validator";
 import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import doctorModel from "../models/doctorModel.js";
+import jwt from "jsonwebtoken";
 
 // Api for adding doctor
 const addDoctor = async (req, res) => {
@@ -84,4 +85,24 @@ const addDoctor = async (req, res) => {
   }
 };
 
-export { addDoctor };
+// api for the admin login
+const loginAdmin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "invalid Credentials." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { addDoctor, loginAdmin };
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { addDoctor } from "../controllers/adminController.js";
+import { addDoctor, loginAdmin } from "../controllers/adminController.js";
 import upload from "../middlewares/multer.js";
+import authAdmin from "../middlewares/authAdmin.js";
 
 const adminRouter = express.Router();
 
-adminRouter.post("/add-doctor", upload.single("image"), addDoctor);
+adminRouter.post("/add-doctor", authAdmin, upload.single("image"), addDoctor);
+adminRouter.post("/login", loginAdmin);
 export default adminRouter;
