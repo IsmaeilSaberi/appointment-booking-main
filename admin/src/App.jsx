45f2e5e698d358e33de7ff83a5c547349fe7df62
Ismaeil Saberi <ismaeilsaberi@gmail.com>
2024-10-09 08:06:56 -0700

    adding assets and login page to the admin project

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Login from "./pages/login";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 };
 
 export default App;
