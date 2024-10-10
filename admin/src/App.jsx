9a06b12efe86802fbd3d668691e88a291e3f49f6
Ismaeil Saberi <ismaeilsaberi@gmail.com>
2024-10-09 20:53:33 -0700

    adding admin login successfully

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Login from "./pages/login";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <div>
       <Login />
+      <ToastContainer />
     </div>
   );
 };
