10cd6141738da17ce82dfc2be13d8aee56a43bd3
Ismaeil Saberi <ismaeilsaberi@gmail.com>
2024-10-09 21:17:47 -0700

    setting admin token to localStorage and using it for admin login fuctionality

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import Login from "./pages/login";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { AdminContext } from "./context/AdminContext";
 
 const App = () => {
-  return (
+  const { aToken } = useContext(AdminContext);
+  return aToken ? (
     <div>
-      <Login />
       <ToastContainer />
     </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
   );
 };
 
--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -3,7 +3,9 @@ import { createContext, useState } from "react";
 export const AdminContext = createContext();
 
 const AdminContextProvider = (props) => {
-  const [aToken, setAToken] = useState("");
+  const [aToken, setAToken] = useState(
+    localStorage.getItem("aToken") ? localStorage.getItem("aToken") : ""
+  );
 
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
