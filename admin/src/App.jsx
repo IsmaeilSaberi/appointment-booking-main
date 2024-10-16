5c7f4ddb03a3218f5a604acff10a07aa35d0d7d0
Ismaeil <ismaeilsaberi@gmail.com>
2024-10-16 11:55:42 -0700

    completeing the sidebar of admin dashboard

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,6 +4,12 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { AdminContext } from "./context/AdminContext";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Admin/Dashboard";
+import AllAppointments from "./pages/Admin/AllAppointments";
+import AddDoctor from "./pages/Admin/AddDoctor";
+import DoctorsList from "./pages/Admin/DoctorsList";
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
@@ -11,6 +17,16 @@ const App = () => {
     <div className="bg-[#F8F9FD]">
       <ToastContainer />
       <Navbar />
+      <div className="flex items-start">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/admin-dashboard" element={<Dashboard />} />
+          <Route path="/all-appointments" element={<AllAppointments />} />
+          <Route path="/add-doctor" element={<AddDoctor />} />
+          <Route path="/doctor-list" element={<DoctorsList />} />
+        </Routes>
+      </div>
     </div>
   ) : (
     <>
---
+++ admin/src/pages/Admin/AllAppointments.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllAppointments = () => {
+  return <div>AllAppointments</div>;
+};
+
+export default AllAppointments;
