ef3f41921c07b83648bd9405da6dc85a57548039
Ismaeil <ismaeilsaberi@gmail.com>
2024-10-16 10:22:47 -0700

    adding Navbar component to the admin project

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,12 +3,14 @@ import Login from "./pages/login";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { AdminContext } from "./context/AdminContext";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
   return aToken ? (
-    <div>
+    <div className="bg-[#F8F9FD]">
       <ToastContainer />
+      <Navbar />
     </div>
   ) : (
     <>
